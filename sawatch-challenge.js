31ef805eb98c870337a19dcbd2c1a9043334455a
github username <github email address>
2019-07-08 14:26:18 -0400

    first finish

--- sawatch-challenge.js
+++ sawatch-challenge.js
@@ -7,4 +7,27 @@ getData = () => {
     .catch(error => console.error(error))
 }
 
+sortVehicles = (vehicles) => {
+  const sortedVehicles = vehicles.sort(compareModels)
+  sortedVehicles.forEach((vehicle) => appendTable(vehicle))
+}
+
+compareModels = (vehicleA, vehicleB) => {
+  if (vehicleA.vehicle_model < vehicleB.vehicle_model)
+     return -1
+  if (vehicleA.vehicle_model > vehicleB.vehicle_model)
+    return 1
+  return 0
+}
+
+appendTable = (vehicle) => {
+  const vehicleRow = vehicleTable.insertRow(-1)
+  vehicleRow.innerHTML = `<td>${vehicle.vehicle_year}</td>
+  <td>${vehicle.make}</td>
+  <td>${vehicle.vehicle_model}</td>
+  <td>${vehicle.displacement}</td>
+  <td>${vehicle.cylinders}</td>
+  <td>${vehicle.class}</td>`
+}
+
 getData()
