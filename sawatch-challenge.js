25e9d7a1f34e7223d9b22e6a3f9a3a38c3b225e8
github username <github email address>
2019-07-08 15:01:52 -0400

    add comments

--- sawatch-challenge.js
+++ sawatch-challenge.js
@@ -1,17 +1,18 @@
-const vehicleTable = document.querySelector('.swt-table')
-
+// Get data from API endpoint and invokes sortVehicles with the array of vehicle objects as an argument
 getData = () => {
   return fetch('https://api.sawatchlabs.com/models/13/2017')
     .then(response => response.json())
-    .then(data => sortVehicles(data.data))
+    .then(models => sortVehicles(models.data))
     .catch(error => console.error(error))
 }
 
+// Take array of vehicle objects and sorts them by model, then appends a new row for each vehicle to the table
 sortVehicles = (vehicles) => {
   const sortedVehicles = vehicles.sort(compareModels)
-  sortedVehicles.forEach((vehicle) => appendTable(vehicle))
+  sortedVehicles.forEach((vehicle) => appendVehicleRowToTable(vehicle))
 }
 
+// Compares vehicles by their model to be sorted appropriately
 compareModels = (vehicleA, vehicleB) => {
   if (vehicleA.vehicle_model < vehicleB.vehicle_model)
      return -1
@@ -20,14 +21,16 @@ compareModels = (vehicleA, vehicleB) => {
   return 0
 }
 
-appendTable = (vehicle) => {
+// Append a new row to the table for an instance of a vehicle
+appendVehicleRowToTable = (vehicle) => {
+  const vehicleTable = document.querySelector('.swt-table')
   const vehicleRow = vehicleTable.insertRow(-1)
   vehicleRow.innerHTML = `<td>${vehicle.vehicle_year}</td>
-  <td>${vehicle.make}</td>
-  <td>${vehicle.vehicle_model}</td>
-  <td>${vehicle.displacement}</td>
-  <td>${vehicle.cylinders}</td>
-  <td>${vehicle.class}</td>`
+    <td>${vehicle.make}</td>
+    <td>${vehicle.vehicle_model}</td>
+    <td>${vehicle.displacement}</td>
+    <td>${vehicle.cylinders}</td>
+    <td>${vehicle.class}</td>`
 }
 
 getData()
